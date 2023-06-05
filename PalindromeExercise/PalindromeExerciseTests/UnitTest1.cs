4b2d7e2afdbd8662c2942a5e4e8af56fb7c9f084
Ben Zellers <zellersdenver@gmail.com>
2023-06-05 12:56:23 -0400

    exercise 2 completed

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("tattarrattat", true)]
+        [InlineData("deified", true)]
+        [InlineData("mississippi", false)]
+        [InlineData("antifungal", false)]
 
+
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            var ws = new WordSmith();
+
+            var actual = ws.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmithTests.cs
@@ -0,0 +1,32 @@
+﻿using PalindromeExercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PalindromeExerciseTests
+{
+    public class WordSmithTests
+    {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Racecar", true)]
+        [InlineData("tattarrattat", true)]
+        [InlineData("deified", true)]
+        [InlineData("mississippi", false)]
+        [InlineData("antifungal", false)]
+        
+        
+        public void IsAPalindrome_Test(string word, bool expected)
+        {
+            var ws = new WordSmith();
+
+            var actual = ws.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
